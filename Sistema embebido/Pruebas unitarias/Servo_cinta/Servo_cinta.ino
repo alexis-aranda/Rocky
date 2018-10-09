4b07a4e3ce24ddfd4da6338320ab8247bbac605d
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-10-09 17:40:36 -0300

    Correcciones en primer estacion

--- Sistema embebido/Pruebas unitarias/Servo_cinta/Servo_cinta.ino
+++ Sistema embebido/Pruebas unitarias/Servo_cinta/Servo_cinta.ino
@@ -2,7 +2,7 @@
 
 //const int max = 240; //Servo negro
 const int max = 220; //Prueba servo negro antes del limite
-const int min = 50; //Servo negro
+const int min = 60; //Servo negro
 const int delta = 5;
 
 int pos1;
@@ -12,14 +12,14 @@ int pos3;
 void setup() {
   Serial.begin(9600);
   pinMode(9, OUTPUT);
-  pos1 = map(0, 0, 100, min, max);
+  pos1 = map(10, 0, 100, min, max);
   pos2 = map(50, 0, 100, min, max);
   pos3 = map(100, 0, 100, min, max);
 }
 
 void loop() {
   analogWrite(9, pos1);
-  delay(1500);
+  delay(2000);
   analogWrite(9, pos2);
   delay(1500);
   analogWrite(9, pos3);
