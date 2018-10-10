f695378173953ba7ef6cbc6ebd6c9c861151814e
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-10-10 12:19:51 -0300

    Hardcodeo posiciones del tobogan

--- Sistema embebido/Pruebas unitarias/Servo_tobogan/Servo_tobogan2/Servo_tobogan2.ino
+++ Sistema embebido/Pruebas unitarias/Servo_tobogan/Servo_tobogan2/Servo_tobogan2.ino
@@ -13,12 +13,12 @@ int pos6;
 void setup() {
   Serial.begin(9600);
   pinMode(9, OUTPUT);
-  pos1 = map(0, 0, 180, min, max);
-  pos2 = map(30, 0, 180, min, max);
-  pos3 = map(60, 0, 180, min, max);
-  pos4 = map(100, 0, 180, min, max);
-  pos5 = map(145, 0, 180, min, max);
-  pos6 = map(175, 0, 180, min, max);
+  pos1 = 50; //map(0, 0, 180, min, max);
+  pos2 = 84; //map(30, 0, 180, min, max);
+  pos3 = 118; //map(60, 0, 180, min, max);
+  pos4 = 163; //map(100, 0, 180, min, max);
+  pos5 = 214; //map(145, 0, 180, min, max);
+  pos6 = 248; //map(175, 0, 180, min, max);
 }
 
 void loop() {
