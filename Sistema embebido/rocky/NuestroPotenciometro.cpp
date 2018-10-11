b676ac96cced75f1470cde7c07878b0a4e0c645f
Alexis Aranda Ocampo <31006948+alexis-aranda@users.noreply.github.com>
2018-10-11 13:32:16 -0300

    Errores que me corrigió vale

--- Sistema embebido/rocky/NuestroPotenciometro.cpp
+++ Sistema embebido/rocky/NuestroPotenciometro.cpp
@@ -1,8 +1,9 @@
+#include <Arduino.h>
 #include "NuestroPotenciometro.h"
 
 NuestroPotenciometro::NuestroPotenciometro(const int pin){
 	this->pin=pin;
-	pinMode(this->pin, INPUT); //No recuerdo si este era el formato que se usaba para una entrada analogica, sino corregime vale
+	//pinMode(this->pin, INPUT); //Inncesario
 }
 
 int NuestroPotenciometro::getPosicion(){
@@ -11,6 +12,6 @@ int NuestroPotenciometro::getPosicion(){
 }
 
 int NuestroPotenciometro::getPosicion256(){
-	int value = this->getPosicion * (float)256 / 1024;
+	int value = this->getPosicion() * (float)256 / 1024;
 	return value;
 }
