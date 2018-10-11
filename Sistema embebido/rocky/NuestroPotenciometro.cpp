f1c47475dc9e824e1118059a5397915ee668a8ac
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-10-11 16:36:03 -0300

    Dejen de romper cosas con los commits
    No se que paso, que en un commit lo arreglamos, en otro se rompio, lo volvimos a arreglar y ahora lo arreglo por tercera vez...
    Ademas parece que a la IDE de Arduino le encanta agregar espacios, que se le va a hacer.

--- Sistema embebido/rocky/NuestroPotenciometro.cpp
+++ Sistema embebido/rocky/NuestroPotenciometro.cpp
@@ -1,8 +1,8 @@
+#include <Arduino.h>
 #include "NuestroPotenciometro.h"
 
 NuestroPotenciometro::NuestroPotenciometro(const int pin){
 	this->pin=pin;
-	pinMode(this->pin, INPUT); //No recuerdo si este era el formato que se usaba para una entrada analogica, sino corregime vale
 }
 
 int NuestroPotenciometro::getPosicion(){
@@ -11,6 +11,6 @@ int NuestroPotenciometro::getPosicion(){
 }
 
 int NuestroPotenciometro::getPosicion256(){
-	int value = this->getPosicion * (float)256 / 1024;
+	int value = this->getPosicion() * (float)256 / 1024;
 	return value;
 }
