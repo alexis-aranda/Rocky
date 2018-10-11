18d8d4ebb8a0bfa47352b6b687fc09d9d9c1fc00
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-10-11 16:13:04 -0300

    Pruebo servo cinta con NuestroServo

---
+++ Sistema embebido/Pruebas unitarias/Servo_cinta2_conNuestroServo/Servo_cinta2_conNuestroServo.ino
@@ -0,0 +1,18 @@
+#include "nuestroServo.h"
+
+const int max = 220; //Prueba servo negro antes del limite
+const int min = 60; //Servo negro
+
+NuestroServo serv(9, min, max);
+
+void setup() {
+}
+
+void loop() {
+  serv.irA(NuestroServo::RECEPCION_ST);
+  delay(2000);
+  serv.irA(NuestroServo::COLOR_ST);
+  delay(1500);
+  serv.irA(NuestroServo::CAIDA_ST);
+  delay(1500);
+}
