e18977d1e48128b64cb2aa4e5ef0693c0ec8cdbb
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2018-10-12 13:42:06 -0300

    Ahora si, compilado y guardado, mando actualizado, NuestroLED
    faltan implementar solo las funciones especificas por modo

--- Sistema embebido/rocky/NuestroLED.cpp
+++ Sistema embebido/rocky/NuestroLED.cpp
@@ -11,3 +11,68 @@ NuestroLED::NuestroLED(const int pin, const int modo){
   pinMode(this->pin, OUTPUT);
 	this->modo=modo;
 }
+
+void NuestroLED::setModo(const int modo){
+    this->modo=modo;
+    /* Probablemente necesite revisar si necesito resetear alguna variable acá */
+}
+
+int NuestroLED::getModo(){
+    return this->modo;
+}
+
+void NuestroLED::activar(const int intensidadPWMManual){
+     switch ( this->modo )  
+      {  
+         case PRENDE_APAGA:
+            onOff();
+            break;  
+         case INTENSIDAD_VARIABLE:
+            manual(intensidadPWMManual);
+            break;
+         case SOFT_PWM:
+            soft();
+            break;
+         case SIEMPRE_PRENDIDO:
+            on();
+            break;
+         case SIEMPRE_APAGADO:
+            off();
+            break;  
+         default:  
+            break; //IDK
+      }  
+}
+
+void NuestroLED::setIntensidadPWM(){
+  analogWrite(this->pin,this->intensidad);
+}
+
+void NuestroLED::setIntensidadBoolON(){
+  digitalWrite(this->pin,HIGH);
+}
+
+void NuestroLED::setIntensidadBoolOFF(){
+  digitalWrite(this->pin,LOW);
+}
+
+void NuestroLED::onOff(){
+  
+}
+
+void NuestroLED::manual(const int intensidadPWMManual){
+  this->intensidad = intensidadPWMManual;
+  
+}
+
+void NuestroLED::soft(){
+  
+}
+
+void NuestroLED::on(){
+  
+}
+
+void NuestroLED::off(){
+  
+}
--- Sistema embebido/rocky/NuestroLED.h
+++ Sistema embebido/rocky/NuestroLED.h
@@ -2,20 +2,20 @@
 
 class NuestroLED{
 public:
-	static const int LIM_INF=0;
-	static const int LIM_SUP=255;
+  static const int NO_MANUAL=-1; //Usada para cuando no hay necesidad de enviar una intensidad manualmente
+	static const int LIM_INF=0; //Creo que es este
+	static const int LIM_SUP=255 * 90 / 100; //Necesita actualización, no es realmente esto, creo que era el 90%
 	static const int PRENDE_APAGA=0; //Manual, todo el tiempo excepto cuando se usa el tobogan
 	static const int INTENSIDAD_VARIABLE=1; // Manual, cuando se usa el tobogan
 	static const int SOFT_PWM=2; //Auto, esperando rocklets
 	static const int SIEMPRE_PRENDIDO=3; //Auto, al reconocer color
 	static const int SIEMPRE_APAGADO=4; //Auto, al tirar
 
-	NuestroLED(const int pin); //Incializo, por defecto en modo PRENDE_APAGA
+	NuestroLED(const int pin); //Incializo, por defecto en modo SOFT_PWM
 	NuestroLED(const int pin,const int modo); //Inicializo, con modo
 	void setModo(const int modo); //Seteo el modo
 	int getModo(); //Obtengo el modo actual
-	void setIntensidad(int intensidad); //Seteo la intensidad manualmente, talvez quede privada
-	void activar(); //Activo el LED, de forma generica, la idea es que sea transparente por cada ciclo, solo según el modo
+	void activar(const int intensidadPWMManual); //Activo el LED, de forma generica, la idea es que sea transparente por cada ciclo, solo según el modo
 
 private:
 	int pin;
@@ -23,4 +23,12 @@ private:
 	int intensidad; //Intensidad actual
 	bool enSubida; //Para el modo SOFT_PWM
 	bool isON; //Para el modo PRENDE_APAGA
+  void setIntensidadPWM(); //Cambio de intensidad en el LED real, solo privada
+  void setIntensidadBoolON(); //Cambio de intensidad en el LED real, solo privada
+  void setIntensidadBoolOFF(); //Cambio de intensidad en el LED real, solo privada
+  void onOff(); //Va prendiendo y apagando cada cierto tiempo
+  void manual(const int intensidadPWMManual); //Modo manual, recibe de un potenciometro
+  void soft(); //Varía subiendo y apagando el PWM
+  void on(); //Se mantiene encendido
+  void off(); //Se mantiene apagado
 };
