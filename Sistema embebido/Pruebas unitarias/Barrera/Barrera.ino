a46fae287a887dbfbff9e961bcfcb9d3c3c63735
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-10-31 15:25:11 -0300

    Prueba de barrera laser

---
+++ Sistema embebido/Pruebas unitarias/Barrera/Barrera.ino
@@ -0,0 +1,16 @@
+#include "NuestraBarreraLaser.h"
+
+NuestraBarreraLaser barrera(2,3);
+int pinLed = 13;
+
+void setup(){
+  barrera.activarBarrera();
+  pinMode(pinLed, OUTPUT);
+}
+
+void loop(){
+  if(barrera.detecta())
+    digitalWrite(pinLed, HIGH);
+   else
+    digitalWrite(pinLed, LOW);
+}
