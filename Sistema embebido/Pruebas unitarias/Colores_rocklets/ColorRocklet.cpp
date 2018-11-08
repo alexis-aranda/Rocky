01ff0ed694682be46662ac965883e9e76485d23e
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-11-08 18:33:40 -0300

    Inicio de prueba de color

---
+++ Sistema embebido/Pruebas unitarias/Colores_rocklets/ColorRocklet.cpp
@@ -0,0 +1,223 @@
+#include <Arduino.h>
+#include "ColorRocklet.h"
+
+ColorRocklet::ColorRocklet(const int pin_s0, const int pin_s1, const int pin_s2, const int pin_s3, const int pin_out){
+    //inicialización de ids y valores
+    this->idColor = NO_IDENTIFICADO;
+    this->rojo = 0;
+    this->verde = 0;
+    this->azul = 0;
+
+    //inicialización de condiciones
+    this->inicio_lectura_loop = true;
+    this->rojoLeido = false;
+    this->azulLeido = false;
+    this->verdeLeido = true;
+	
+	this->nLectura = LEER_ROJO;
+/*
+    // para la 3er alternativa de resolución
+    //inicialización de contador
+    this->cantLoops = 0;
+    // fin 3er alternativa
+*/  
+  //inicialización de pines
+    this->pin_s0 = pin_s0;
+    this->pin_s1 = pin_s1;
+    this->pin_s2 = pin_s2;
+    this->pin_s3 = pin_s3;
+    this->pin_out = pin_out;
+    
+    pinMode(this->pin_s0, OUTPUT);
+    pinMode(this->pin_s1, OUTPUT);
+    pinMode(this->pin_s2, OUTPUT);
+    pinMode(this->pin_s3, OUTPUT);
+    pinMode(this->pin_out, INPUT);
+
+    digitalWrite(this->pin_s0,HIGH);
+    digitalWrite(this->pin_s1,LOW);
+}
+
+void ColorRocklet::setColor(const int idColor){
+    this->idColor=idColor;
+}
+
+//esta opción actúa con una sola lectura
+void ColorRocklet::hacerLectura(){
+
+      if(inicio_lectura_loop){
+        
+        this->tmillis = millis();
+        inicio_lectura_loop = false;
+        
+      }else if ( nLectura == LEER_ROJO && millis() - this->tmillis >= T_ROJO){
+        
+        digitalWrite(this->pin_s2, LOW);  
+        digitalWrite(this->pin_s3, LOW);   
+        this->rojo = pulseIn(this->pin_out, LOW);
+        //this->rojo = pulseIn(this->pin_out, digitalRead(this->pin_out) == HIGH ? LOW : HIGH);  
+        this->tmillis = millis();
+		nLectura = LEER_AZUL;
+        
+      }else if( nLectura == LEER_AZUL && millis() - this->tmillis >= T_AZUL){
+        
+        digitalWrite(this->pin_s3, HIGH);
+        this->azul = pulseIn(this->pin_out, LOW);
+        //this->azul = pulseIn(this->pin_out, digitalRead(this->pin_out) == HIGH ? LOW : HIGH);
+        this->tmillis = millis();
+		nLectura = LEER_VERDE;
+        
+      }else if(nLectura == LEER_VERDE && millis() - this->tmillis >= T_VERDE){
+        
+        digitalWrite(this->pin_s2, HIGH);
+        this->verde = pulseIn(this->pin_out, LOW);
+        //this->verde = pulseIn(this->pin_out, digitalRead(this->pin_out) == HIGH ? LOW : HIGH);  
+        this->tmillis = millis();
+		nLectura = LEER_ROJO;
+        inicio_lectura_loop = true;
+      } 
+}
+  
+void ColorRocklet::identificarColor(){
+    hacerLectura();
+    
+    if((this->verde < this->rojo) && (this->verde < this->azul) && (this->azul < this->rojo)){
+        this->idColor = VERDE;
+        Serial.println("Verde");
+    }else if((this->azul < this->rojo) && (this->azul < this->verde) && (this->verde < this->rojo)){
+        this->idColor = AZUL;
+        Serial.println("Azul");
+    }else if((this->rojo < this->verde) && (this->rojo < this->azul) && (this->verde < this->azul)){
+        this->idColor = AMARILLO;
+        Serial.println("Amarillo");
+    }else if((this->rojo < this->verde) && (this->rojo < this->azul) && (this->azul < this->verde)){
+        if(this->rojo > MIN_MARRON_R){
+           this->idColor = MARRON;
+           Serial.println("Marron");
+        }else if(this->rojo < MAX_NARANJA_R && this->verde < MAX_NARANJA_V){
+          this->idColor = NARANJA;
+          Serial.println("Naranja");
+        }else{
+          this->idColor = ROJO;
+          Serial.println("Rojo");
+        }
+    } else {
+        this->idColor = NO_IDENTIFICADO;
+    }
+    
+    //si no esta dentro de ningun rango, queda como NO_IDENTIFICADO por defecto
+}
+
+int ColorRocklet::getColor(){
+  identificarColor();
+  return this->idColor;
+}
+
+//fin 2da alternativa
+
+/*
+// para la 3er alternativa de resolución que actúa con un contador de N lecturas
+void ColorRocklet::hacerLectura(){
+
+    if(inicio_lectura_loop){
+      this->tmillis = millis();
+      inicio_lectura_loop = false;
+    }else if (millis() - this->tmillis >= T_ROJO && verdeLeido){
+      
+      digitalWrite(this->pin_s2, LOW);  
+      digitalWrite(this->pin_s3, LOW);   
+      this->rojo = this->rojo + pulseIn(this->pin_out, LOW);
+      this->tmillis = millis();
+      rojoLeido = true;
+      verdeLeido = false;
+      
+    }else if(millis() - this->tmillis >= T_AZUL && rojoLeido  && verdeLeido){
+      
+      digitalWrite(this->pin_s3, HIGH);
+      this->azul = this->azul + pulseIn(this->pin_out, LOW);
+      this->tmillis = millis();
+      azulLeido = true;
+      
+    }else if(millis() - this->tmillis >= T_VERDE && azulLeido){
+      
+      digitalWrite(this->pin_s2, HIGH);
+      this->verde = this->verde + pulseIn(this->pin_out, LOW);
+      this->cantLoops += 1; //incremento loop de lectura
+      this->tmillis = millis();
+      verdeLeido = true;
+      
+      rojoLeido = false;
+      azulLeido = false;
+      inicio_lectura_loop = true;
+    }
+}
+
+void ColorRocklet::obtenerPromedio(){
+    this->rojo = this->rojo / this->cantLoops;
+    this->verde = this->verde / this->cantLoops;
+    this->azul = this->azul / this->cantLoops;
+}
+
+void ColorRocklet::identificarColor(){
+    if(this->cantLoops < LOOPS_COLOR){
+      hacerLectura();
+    }else{
+      obtenerPromedio();
+      
+      if((this->verde < this->rojo) && (this->verde < this->azul) && (this->azul < this->rojo)){
+          this->idColor = VERDE;
+      }else if((this->azul < this->rojo) && (this->azul < this->verde) && (this->verde < this->rojo)){
+          this->idColor = AZUL;
+      }else if((this->rojo < this->verde) && (this->rojo < this->azul) && (this->verde < this->azul)){
+          this->idColor = AMARILLO;
+      }else if((this->rojo < this->verde) && (this->rojo < this->azul) && (this->azul < this->verde)){
+          if(this->rojo > MIN_MARRON_R){
+             this->idColor = MARRON;
+          }else if(this->rojo < MAX_NARANJA_R && this->verde < MAX_NARANJA_V){
+            this->idColor = NARANJA;
+          }else{
+            this->idColor = ROJO;
+          }
+      }
+      //después de N lecturas debería tener identificado el color
+      this->cantLoops = 0;
+    }
+    
+    //si no esta dentro de ningun rango, queda como NO_IDENTIFICADO por defecto
+}
+// fin 3er alternativa
+*/
+
+/*
+//esta es la primer resolución que plantee con el uso de vecores con las medias por color
+bool ColorRocklet::enRango(const int vecCol[]){
+    return ((this->rojo>=vecCol[0] - DESVIO_COLOR && this->rojo<=vecCol[0] + DESVIO_COLOR)
+    && (this->verde>=vecCol[1] - DESVIO_COLOR && this->verde<=vecCol[1] + DESVIO_COLOR)
+    && (this->azul>=vecCol[2] - DESVIO_COLOR && this->azul<=vecCol[2] + DESVIO_COLOR));
+}
+
+void ColorRocklet::identificarColor(){
+    hacerLectura();
+    
+    if(enRango(M_VERDE)){
+        this->idColor = VERDE;
+        return;
+    }else if(enRango(M_AZUL)){
+        this->idColor = AZUL;
+        return;
+    }else if(enRango(M_AMARILLO)){
+        this->idColor = AMARILLO;
+        return;
+    }else if(enRango(M_ROJO)){
+        this->idColor = ROJO;
+        return;
+    }else if(enRango(M_NARANJA)){
+        this->idColor = NARANJA;
+        return;
+    }else if(enRango(M_MARRON)){
+        this->idColor = MARRON;
+        return;
+    }
+    //si no esta dentro de ningun rango, queda como NO_IDENTIFICADO por defecto
+}
+*/
---
+++ Sistema embebido/Pruebas unitarias/Colores_rocklets/ColorRocklet.h
@@ -0,0 +1,95 @@
+/*
+    ColorRocklet.h - Biblioteca para identificar el color de un rocklet
+*/
+#include <Arduino.h>
+#ifndef ColorRocklet_h
+#define ColorRocklet_h
+/*
+#ifndef LECTURAS_X_COLOR
+#define LECTURAS_X_COLOR 3
+#define DESVIO_COLOR 10
+*/
+
+class ColorRocklet{
+	public:
+	    /* Definición de id para los Colores */
+		static const int NO_IDENTIFICADO = 0; //para cuando no reconoce ningún color
+		static const int VERDE = 1;
+		static const int AZUL = 2;
+		static const int ROJO = 3;
+		static const int NARANJA = 4;
+		static const int AMARILLO = 5;
+		static const int MARRON = 6;
+		static const int LOOPS_COLOR = 10; // para la alternativa de resolución
+		
+		static const int LEER_ROJO = 1;
+		static const int LEER_AZUL = 2;
+		static const int LEER_VERDE = 3;
+
+		/* Defino las medias de valores para cada color */
+		/* MEDIA_COLOR[] = {rojo, verde, azul};*/
+		/*
+		static const int M_VERDE[LECTURAS_X_COLOR] = {155,142,154};
+		static const int M_AZUL[LECTURAS_X_COLOR] = {169,156,111};
+		static const int M_ROJO[LECTURAS_X_COLOR] = {135,231,183};
+		static const int M_NARANJA[LECTURAS_X_COLOR] = {100,183,163};
+		static const int M_AMARILLO[LECTURAS_X_COLOR] = {98,138,158};
+		static const int M_MARRON[LECTURAS_X_COLOR] = {182,233,188};*/
+
+    /* Defino topes para los colores en conflicto*/
+    static const int MAX_NARANJA_R = 130;
+    static const int MAX_NARANJA_V = 200;
+    static const int MIN_MARRON_R = 160;
+
+    /* 
+     * Inicializo con los pines que me asignen.
+     * Inicializo como NO_IDENTIFICADO por defecto.
+     * Inicializo parámetros de colores en 0.
+    */
+		ColorRocklet(const int pin_s0, const int pin_s1, const int pin_s2, const int pin_s3, const int pin_out);
+
+    /*
+     * Llama a identificarColor().
+     * Devuelve el id del color identificado.
+     */
+		int getColor();
+
+	private:
+		int idColor; //id actual
+		int rojo; //rojo medido actual
+		int verde; //verde medido actual
+		int azul; //azul medido actual
+		int nLectura;
+
+    /* pines del sensor de color */
+    int pin_s0;
+    int pin_s1;
+    int pin_s2;
+    int pin_s3;
+    int pin_out;
+
+    bool inicio_lectura_loop;
+    bool rojoLeido;
+    bool azulLeido;
+    bool verdeLeido;
+    static const unsigned long T_ROJO = 100; //Tiempo de espera entre lecturas
+    static const unsigned long T_AZUL = 100; //Tiempo de espera entre lecturas
+    static const unsigned long T_VERDE = 100; //Tiempo de espera entre lecturas
+    unsigned long tmillis = 0; //Contador de espera entre lecturas
+    /*
+    // para la 3er alternativa de resolución con millis y un contador de loops para la identificación del color
+    int cantLoops; //cuenta los loops de lectura de datos, vuelve a 0 cuando identifica el color
+    
+    void obtenerPromedio();// para la alternativa de resolución //saca el promedio de todas las lecturas
+    //fin de la 3er alternativa
+    */
+    void identificarColor();//llama a hacerLectura identifica el color según los parámetros leídos.
+    void hacerLectura();//realiza la lectura de datos recibidos desde el sensor y los pone en los parámetros de los colores.
+    void setColor(const int idColor); //por si es necesario
+    
+    
+    /*bool enRango(const int vecColor[]); *//*verifica que los valores esten dentro de un
+                                         *rango indicado (equivalente a un color)*/
+};
+
+#endif
