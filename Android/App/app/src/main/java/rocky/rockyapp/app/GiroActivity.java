56a4303c815506a778102eef7340c3509ce89d70
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2018-11-18 18:26:35 -0300

    Creo la activity de la pantalla para girar el servo manualmente con el celu

---
+++ Android/App/app/src/main/java/rocky/rockyapp/app/GiroActivity.java
@@ -0,0 +1,13 @@
+package rocky.rockyapp.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GiroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_giro);
+    }
+}
