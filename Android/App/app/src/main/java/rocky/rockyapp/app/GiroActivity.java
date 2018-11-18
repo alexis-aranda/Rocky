036916adde5b004a0fbb00bc304293995ba243e5
Alexis Aranda Ocampo <alexis_aranda_ocampo@hotmail.com>
2018-11-18 19:23:50 -0300

    Añadido mensaje en la pantalla de giro manual
     y modo en la pantalla de funciones

--- Android/App/app/src/main/java/rocky/rockyapp/app/GiroActivity.java
+++ Android/App/app/src/main/java/rocky/rockyapp/app/GiroActivity.java
@@ -1,13 +1,26 @@
 package rocky.rockyapp.app;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class GiroActivity extends AppCompatActivity {
+public class GiroActivity extends AppCompatActivity implements SensorEventListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giro);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
