a2838ef1335e5972ac8c50495d8bef7eba29000c
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-11-19 20:21:11 -0300

    Refactoreo potenciometro

--- Sistema embebido/rocky/NuestroPotenciometro.h
+++ Sistema embebido/rocky/NuestroPotenciometro.h
@@ -1,13 +1,9 @@
 /* Potenciometro: Definición de la clase */
 
 class NuestroPotenciometro{
-public:
-	static const int LIMITE_INFERIOR=0; //Solamente por si hay necesidad de usarlo
-	static const int LIMITE_SUPERIOR=1023;
-
-	NuestroPotenciometro(const int pin);
-	int getPosicion();
-	int getPosicion256();
-private:
-	int pin;
+    public:    
+    	NuestroPotenciometro(const int pin);
+    	int getPosicion();
+    private:
+    	int pin;
 };
