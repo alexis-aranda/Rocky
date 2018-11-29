722894144503d63955ba325e812596ba3a1d6afc
Alexis Aranda Ocampo <31006948+alexis-aranda@users.noreply.github.com>
2018-11-29 09:44:03 -0300

    Quito métodos sin usar de Tools

--- Android/App/app/src/main/java/rocky/rockyapp/app/Tools.java
+++ Android/App/app/src/main/java/rocky/rockyapp/app/Tools.java
@@ -7,27 +7,11 @@ class Tools {
     public static final int SALTO_GRIS = 1118481;
     public static final int MUCHA_LUX = 600;
 
-    static public int luxToColor(float luxValue){
-        if(luxValue > 100)
-            luxValue = 100;
-        int colorFinal = (int)((float)16777216 / 100 * luxValue );
-        if(colorFinal == 0) return -1;
-        return colorFinal * -1;
-    }
-
     static public int luxToGreyColor(float luxValue){
         if(luxValue >= MUCHA_LUX)
             return Color.WHITE;
         int lightPer = (int)(luxValue / MUCHA_LUX * 16);
         return Color.BLACK + SALTO_GRIS * lightPer;
-    }
-
-    static public int gyroToServo(float gyroValue){
-        if(gyroValue >= 0.5)
-            return 1023;
-        if(gyroValue <= -0.5)
-            return 0;
-        return (int)((gyroValue + 0.5) * 1024 );
     }
 
     static public byte gyroToServo256(float gyroValue){
@@ -36,14 +20,6 @@ class Tools {
         if(gyroValue <= -0.5)
             return 0;
         return (byte)(int)((gyroValue + 0.5) * 256 );
-    }
-
-    static public int gyroToServoInverse(float gyroValue){ //Inversa, por si el servo gira al reves, solo dejar la que se valla a usar
-        if(gyroValue >= 0.5)
-            return 0;
-        if(gyroValue <= -0.5)
-            return 1023;
-        return (int)((gyroValue - 0.5) * -1024 );
     }
 
     static public String codToStringColor(char value){
